ce4877b011b3949855c86d1f32479c3eec9d595e
Erick Gomez Lorenzana <erickg.lorenzana@gmail.com>
2019-08-16 19:46:49 -0500

    Adds initial code for floating action button for create todos

---
+++ app/components/CreateTodoFAB.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { FloatingAction } from 'react-native-floating-action';
+import FAIcon from 'react-native-vector-icons/FontAwesome';
+import MDIcon from 'react-native-vector-icons/MaterialDesign';
+import { actionsForMainFAB } from 'utils/todoListUtils';
+
+
+const actions = actionsForMainFAB.map((action) => {
+  const { iconName, material, ...rest } = action;
+  const Icon = material ? MDIcon : FAIcon;
+  return {
+    ...rest,
+    icon: <Icon name={iconName} />
+  };
+});
+
+export default function CreateTodoFAB(props) {
+  const navigateToCreate = function(params = {}) {
+    // code for navigation and pass the params, something like:
+    // props.navigation.navigate(screenName, params)
+  }
+  
+  const handleSelectAction = function(actionName) {
+    navigateToCreate({ dateKey: actionName });
+  }
+  
+  return (
+    <FloatingAction
+      actions={actions}
+      onPressItem={handleSelectAction}
+    />
+  );
+}
